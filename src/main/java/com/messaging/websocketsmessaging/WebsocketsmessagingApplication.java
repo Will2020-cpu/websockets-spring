918531cf27a1bd2d9b028d814c5a6da4527eb3c3
madara <wcamacho221@gmail.com>
2021-06-24 03:41:14 -0500

    Microservices chat

---
+++ src/main/java/com/messaging/websocketsmessaging/WebsocketsmessagingApplication.java
@@ -0,0 +1,13 @@
+package com.messaging.websocketsmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsmessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsmessagingApplication.class, args);
+	}
+
+}
